f801a46a119e35484c968fa5b16a9c970c51abfe
Raghuveer D <raghuveer.d@blueyonder.com>
2021-02-27 22:05:29 +0530

    ArrayChunking first cut

---
+++ src/main/java/dr/udemy/ArrayChunking.java
@@ -0,0 +1,80 @@
+package dr.udemy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayChunking {
+
+    public List<Integer[]> getChunksUsingList(int[] input, int chunkSize) {
+
+        int length = input.length;
+        int remainder = input.length % chunkSize;
+
+        if (remainder > 0) {
+            length = length - remainder;
+        }
+
+        List<Integer[]> out = new ArrayList<>();
+
+        Integer[] temp = new Integer[0];
+        int tempChunkSize = chunkSize;
+
+        for (int i = 0; i < length; i++) {
+            if (tempChunkSize == chunkSize) {
+                tempChunkSize = 0;
+                temp = new Integer[chunkSize];
+                out.add(temp);
+            }
+            temp[tempChunkSize] = input[i];
+            tempChunkSize++;
+        }
+
+        if (remainder > 0) {
+            temp = new Integer[remainder];
+            int start = (input.length / chunkSize) * chunkSize;
+            for (int i = 0; i < remainder; i++) {
+                temp[i] = input[start + i];
+            }
+            out.add(temp);
+        }
+        return out;
+    }
+
+
+    public int[][] getChunksUsingArrays(int[] input, int chunkSize) {
+
+        int length = input.length;
+        int remainder = input.length % chunkSize;
+        int numChunks = input.length / chunkSize;
+
+        if (remainder > 0) {
+            numChunks++;
+            length = length - remainder;
+        }
+
+        int[][] out = new int[numChunks][];
+
+        int tempChunkSize = chunkSize;
+        int[] temp = new int[0];
+
+        int curChunk = 0;
+
+        for (int i = 0; i < length; i++) {
+            if (tempChunkSize == chunkSize) {
+                tempChunkSize = 0;
+                temp = new int[chunkSize];
+                out[curChunk] = temp;
+                curChunk++;
+            }
+            temp[tempChunkSize] = input[i];
+            tempChunkSize++;
+        }
+        if (remainder > 0) {
+            temp = new int[remainder];
+            int start = (input.length / chunkSize) * chunkSize;
+            System.arraycopy(input, start, temp, 0, remainder);
+            out[curChunk] = temp;
+        }
+        return out;
+    }
+}
---
+++ src/test/java/dr/udemy/ArrayChunkingTest.java
@@ -0,0 +1,41 @@
+package dr.udemy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class ArrayChunkingTest {
+    @Test
+    public void testGetChunks() {
+        ArrayChunking chunking = new ArrayChunking();
+        int[] input1 = {1, 2, 3, 4, 5, 6};
+        List<Integer[]> chunks = chunking.getChunksUsingList(input1, 4);
+        Assertions.assertEquals(2, chunks.size());
+        Assertions.assertEquals(4, chunks.get(0).length);
+        Assertions.assertEquals(2, chunks.get(1).length);
+    }
+
+    @Test
+    public void testGetChunksUsingArrays() {
+        ArrayChunking chunking = new ArrayChunking();
+        int[] input1 = {1, 2, 3, 4, 5, 6};
+        int[][] chunks1 = chunking.getChunksUsingArrays(input1, 4);
+        Assertions.assertEquals(2, chunks1.length);
+        Assertions.assertEquals(4, chunks1[0].length);
+        Assertions.assertEquals(2, chunks1[1].length);
+
+        int[] input2 = {1, 2, 3, 4, 5, 6};
+        int[][] chunks2 = chunking.getChunksUsingArrays(input2, 3);
+        Assertions.assertEquals(2, chunks2.length);
+        Assertions.assertEquals(3, chunks2[0].length);
+        Assertions.assertEquals(3, chunks2[1].length);
+
+        int[] input3 = {1, 2, 3, 4, 5, 6, 7};
+        int[][] chunks3 = chunking.getChunksUsingArrays(input3, 3);
+        Assertions.assertEquals(3, chunks3.length);
+        Assertions.assertEquals(3, chunks3[0].length);
+        Assertions.assertEquals(3, chunks3[1].length);
+        Assertions.assertEquals(1, chunks3[2].length);
+    }
+}
